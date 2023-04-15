e9ba90b8524708af58d0aeb62743d4b306c79bce
Muslum Abd Ali <muslumabdali1234@gmail.com>
2023-04-15 01:12:57 -0400

    Automated textCopy

--- textCopy.py
+++ textCopy.py
@@ -1,6 +1,7 @@
 import pyautogui
 import pytesseract
 from PIL import Image
+import time
 
 # Define the region of the screen to capture
 x, y, width, height = 242, 47, 745, 121
@@ -24,25 +25,37 @@ routePokemonDict = {
 class ImageDiscover:
     def __init__(self, cordsDictionary):
         self.dict = cordsDictionary
+        self.oldtext = ''
     def takeScreenshot(self, section_name):
-        print(section_name)
         self.section = cordsDictionary[section_name]
         x, y, width, height = self.section[0], self.section[1], self.section[2], self.section[3]
         screenshot = pyautogui.screenshot(region=(x, y, width, height))
         screenshot.save(f'{section_name}Image.png')
+
     def screenshotAnalyze(self, requestedImage):
+
         imageGiven = Image.open(requestedImage)
         text = pytesseract.image_to_string(imageGiven)
-        print(text)
+
+        if self.oldtext != text:
+            print(text)
+            self.oldtext = text
 
 
+
+    def takeAnalyzeLoop(self, requestedImage, section_name):
+        while True:
+            self.takeScreenshot(section_name)
+            self.screenshotAnalyze(requestedImage
+                                   )
+            time.sleep(0.1)
 
 
 
 
 
 ia = ImageDiscover(cordsDictionary)
-ia.screenshotAnalyze('routeImage.png')
+ia.takeAnalyzeLoop('routeImage.png', 'Route')
 
 """
 ib = ImageAnalyze(cordsDictionary)
