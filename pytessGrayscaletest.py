56214f60e54a9c50fadce7eafc4f1e8f309a77ac
Muslum Abd Ali <muslumabdali1234@gmail.com>
2023-04-26 14:29:20 -0400

    changed pytessGrayscaletest to class

--- pytessGrayscaletest.py
+++ pytessGrayscaletest.py
@@ -14,3 +14,11 @@ image = ImageEnhance.Contrast(image).enhance(2.0) # adjust the factor as needed
 text = pytesseract.image_to_string(image)
 
 print(text)
+
+
+class imageEnhancer:
+    def enhanceFunction(imageToEnhance):
+        img = Image.open(imageToEnhance)
+        img = image.convert('L')
+        img = ImageEnhance.Contrast(img).enhance(2.0)
+        return img
