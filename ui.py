25e9fec1ac5af51aa5b82c3fea30461aa851445c
Muslum Abd Ali <muslumabdali1234@gmail.com>
2023-05-06 07:50:46 -0400

    implementation of ui

--- ui.py
+++ ui.py
@@ -1,6 +1,58 @@
 import json
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QPushButton, QVBoxLayout, QWidget
+from textCopy import ImageDiscover
+from PyQt5.QtCore import QTimer
 
+cordsDictionary = {
+    'Route':[242, 47, 745, 121],
+    'Pokemon':[300, 110, 450, 121],
+    'Caught':[270, 800, 380, 207]
+}
+
+routePokemonDict = {
+    'PALLET TOWN': None,
+    'ROUTE 1': None,
+    'VIRIDIAN CITY': None,
+    'ROUTE 22': None,
+    'ROUTE 2': None,
+    'VIRIDIAN FOREST': None,
+    'ROUTE 3': None,
+    'ROUTE 4': None,
+    'MT. MOON': None,
+    'CERULEAN CITY': None,
+    'ROUTE 24': None,
+    'ROUTE 25': None,
+    'ROUTE 5': None,
+    'ROUTE 6': None,
+    'VERMILION CITY': None,
+    'ROUTE 11': None,
+    'DIGLETTS CAVE': None,
+    'ROUTE 9': None,
+    'ROUTE 10': None,
+    'ROCK TUNNEL': None,
+    'POKÉMON TOWER': None,
+    'ROUTE 12': None,
+    'ROUTE 8': None,
+    'ROUTE 7': None,
+    'CELADON CITY': None,
+    'SAFFRON CITY': None,
+    'ROUTE 16': None,
+    'ONE ISLAND':None,
+    'TWO ISLAND':None,
+    'THREE ISLAND':None,
+    'BERRY FOREST':None,
+    'BOND BRIDGE':None,
+    'MOUNT EMBER':None,
+    'ROUTE 18':None,
+    'ROUTE 20':None,
+    'KINDLE ROAD':None,
+    'ROUTE 18':None,
+    'SAFARI ZONE':None,
+    'ROUTE 15':None
+
+}
+
+ab = ImageDiscover(cordsDictionary=cordsDictionary,routeDict=routePokemonDict)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -19,6 +71,9 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(central_widget)
         self.data = {}
         self.load_json_file()
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.screenShotloop)
+        self.timer.start(100)
 
     def load_json_file(self):
         with open('data.json', 'r') as f:
@@ -42,9 +97,16 @@ class MainWindow(QMainWindow):
         with open('data.json', 'w') as f:
             json.dump(self.data, f, indent=4)
 
+    def screenShotloop(self):
+        ab.takeScreenshot('Route')
+        print("taken")
+
+
+
 
 if __name__ == '__main__':
     app = QApplication([])
     window = MainWindow()
     window.show()
     app.exec_()
+
